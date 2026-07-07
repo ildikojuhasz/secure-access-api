b9acfb51ba59d538bddcd7d3bd7b794370e49e99
ildikojuhasz <juhaszildiko.job@gmail.com>
2026-07-07 07:25:30 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/example/secureaccessapi/SecureAccessApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureaccessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAccessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAccessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secureaccessapi/SecureAccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureaccessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAccessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
